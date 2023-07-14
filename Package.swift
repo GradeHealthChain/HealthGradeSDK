dea6bddb0864576c64dccddc97aaca217fce63ab
Cho sungbok <gging200@gradehc.com>
2023-07-14 13:56:23 +0900

    remove target

--- Package.swift
+++ Package.swift
@@ -10,29 +10,30 @@ let package = Package(
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "HealthGrade",
-            targets: ["HealthGrade", "HealthGradeSDK"]),
+//            targets: ["HealthGrade", "HealthGradeSDK"]),
+            targets: ["HealthGradeSDK"])
     ],
-    dependencies: [
-        .package(url: "https://github.com/Alamofire/Alamofire.git", from: "5.0.0"),
-        .package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", from: "1.0.0"),
-        .package(url: "https://github.com/SwiftyJSON/SwiftyJSON.git", from: "5.0.0"),
-        .package(url: "https://github.com/tid-kijyun/Kanna.git", from: "5.0.0"),
-        .package(url: "https://github.com/svdo/swift-SecurityExtensions.git", from: "4.0.0"),
-        .package(url: "https://github.com/dapperstout/swift-bytes.git", from: "0.0.0"),
-    ],
+//    dependencies: [
+//        .package(url: "https://github.com/Alamofire/Alamofire.git", from: "5.0.0"),
+//        .package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", from: "1.0.0"),
+//        .package(url: "https://github.com/SwiftyJSON/SwiftyJSON.git", from: "5.0.0"),
+//        .package(url: "https://github.com/tid-kijyun/Kanna.git", from: "5.0.0"),
+//        .package(url: "https://github.com/svdo/swift-SecurityExtensions.git", from: "4.0.0"),
+//        .package(url: "https://github.com/dapperstout/swift-bytes.git", from: "0.0.0"),
+//    ],
     targets: [
-        .target(
-            name: "HealthGrade",
-            dependencies: [
-                "Alamofire",
-                "CryptoSwift",
-                "Kanna",
-                "SwiftyJSON",
-                .product(name: "SecurityExtensions", package: "swift-SecurityExtensions"),
-                .product(name: "SwiftBytes", package: "swift-bytes")
-            ],
-            path: "HealthGrade"
-        ),
+//        .target(
+//            name: "HealthGrade",
+//            dependencies: [
+//                "Alamofire",
+//                "CryptoSwift",
+//                "Kanna",
+//                "SwiftyJSON",
+//                .product(name: "SecurityExtensions", package: "swift-SecurityExtensions"),
+//                .product(name: "SwiftBytes", package: "swift-bytes")
+//            ],
+//            path: "HealthGrade"
+//        ),
         .binaryTarget(
             name: "HealthGradeSDK",
             path: "HealthGradeSDK.xcframework"
